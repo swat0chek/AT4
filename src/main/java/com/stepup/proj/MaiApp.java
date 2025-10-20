0a4d867d4e3449dccc23b073594a1023b601525c
Denis Korotov <korotov.deniz@yandex.ru>
2025-10-20 19:02:09 +0300

    Доработка нового поля

--- src/main/java/com/stepup/proj/MaiApp.java
+++ src/main/java/com/stepup/proj/MaiApp.java
@@ -3,5 +3,6 @@ package com.stepup.proj;
 public class MaiApp {
     public static void main(String[] args) {
         System.out.println("Привет Мир");
+        System.out.println(112233);
     }
 }
