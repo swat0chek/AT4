0b0968f29fe74e89f2524a0b116f8ef3d5bbff2b
Denis Korotov <korotov.deniz@yandex.ru>
2025-10-20 19:28:52 +0300

    Преобразование

--- src/main/java/com/stepup/proj/MaiApp.java
+++ src/main/java/com/stepup/proj/MaiApp.java
@@ -2,17 +2,8 @@ package com.stepup.proj;
 
 public class MaiApp {
     public static void main(String[] args) {
-        System.out.println("Привет Мир");
+        System.out.println("Привет Мир, а нет, пока");
         System.out.println(112233);
-    }
-
-    public static void doTwo(String[] args) {
-        System.out.println(3);
-        int a,b,c;
-        a = 3;
-        b = 4;
-        c = a + b;
-        System.out.println(c);
-
+        System.out.println(112234);
     }
 }
--- src/main/java/com/stepup/proj/TwoClass.java
+++ src/main/java/com/stepup/proj/TwoClass.java
@@ -2,7 +2,7 @@ package com.stepup.proj;
 
 public class TwoClass {
     public static void main(String[] args) {
-        System.out.println(34);
+        System.out.println(134);
     }
 
 }
