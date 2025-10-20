e52ed171730e5d6bccc14d56b40596ceb4c067df
Denis Korotov <korotov.deniz@yandex.ru>
2025-10-20 20:17:43 +0300

    Добавлен новый вызов

--- src/main/java/com/stepup/proj/MaiApp.java
+++ src/main/java/com/stepup/proj/MaiApp.java
@@ -4,15 +4,16 @@ public class MaiApp {
     public static void main(String[] args) {
         System.out.println("Привет Мир, а нет, пока");
         System.out.println(112233);
+        doTwo();
         System.out.println(112233);
     }
 
 
-    public static void doTwo(String[] args) {
+    public static void doTwo() {
     int FirstPlace, TwoPlace, ThreePlace;
     FirstPlace = 3;
     TwoPlace = 4;
-    ThreePlace = FirstPlace + TwoPlace;
+    ThreePlace = FirstPlace + TwoPlace - 1;
 
     }
 }
